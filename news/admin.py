478b44aa671b5dce9dc06ac24586950665a3265c
Nekr0bz <Nekr0bz@yandex.ru>
2017-03-02 23:26:23 +0300

    Загрузка картинок для новостей на сервер

--- news/admin.py
+++ news/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 
 from .models import News
 
+
 class NewsAdmin(admin.ModelAdmin):
      prepopulated_fields = {"slug": ("title",)}
 
--- news/models.py
+++ news/models.py
@@ -1,9 +1,6 @@
 # -*- coding: utf-8 -*-
 from django.db import models
-from django.utils import timezone
-from django.dispatch import receiver
-from django.db.models.signals import pre_save
-from ckeditor.fields import RichTextField
+from ckeditor_uploader.fields import RichTextUploadingField
 
 from accounts.models import User
 
@@ -12,7 +9,7 @@ class News(models.Model):
     usr = models.ForeignKey(User)
     title = models.CharField('Заголовок', max_length=80, unique_for_date='datetime')
     description = models.TextField('Краткое описание', max_length=200)
-    content = RichTextField(verbose_name='Основной контент')
+    content = RichTextUploadingField(verbose_name='Основной контент')
     datetime = models.DateTimeField('Опубликована', db_index=True)
     slug = models.SlugField(unique=True)
 
