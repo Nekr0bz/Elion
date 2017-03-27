6a6a281391d1086be0f0a07fc3c258ac818d7ed5
Nekr0bz <Nekr0bz@yandex.ru>
2017-03-27 14:37:41 +0300

    Верстка страницы 'Контакты'

--- Elion/urls.py
+++ Elion/urls.py
@@ -1,14 +1,17 @@
 from django.conf.urls import url, include, static
 from django.contrib import admin
+from django.views.generic.base import TemplateView
+from main import views as main_views
 from . import settings
 
-from main import views
+
 
 urlpatterns = [
-    url(r'^$', views.MainPageView.as_view(), name='main'),
+    url(r'^$', main_views.MainPageView.as_view(), name='main'),
     url(r'^admin/', admin.site.urls),
     url(r'^services/', include('services.urls', namespace='services')),
     url(r'^accounts/', include('accounts.urls', namespace='accounts')),
+    url(r'^contacts/$', TemplateView.as_view(template_name='about/contact.html'), name='contact'),
     url(r'^news/', include('news.urls', namespace='news')),
     url(r'^guestbook/', include('guestbook.urls', namespace='guestbook')),
     url(r'^ckeditor/', include('ckeditor_uploader.urls')),
