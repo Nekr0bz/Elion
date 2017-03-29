1a09e62ed7db4b856ebd2ebc68eedde9ad766761
Nekr0bz <Nekr0bz@yandex.ru>
2017-03-29 18:01:13 +0300

    Заготовка для представления списка новостей

--- news/urls.py
+++ news/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.NewsListViews.as_view(), name="index"),
+    url(r'^$', views.NewsListViews.as_view(), name="list"),
     url(r'^(?P<slug>[-\w]+)/$', views.NewsDetailViews.as_view(), name="detail"),
 ]
--- news/views.py
+++ news/views.py
@@ -7,7 +7,9 @@ from .models import News
 class NewsListViews(ArchiveIndexView):
     model = News
     date_field = 'datetime'
-    template_name = 'news/index.html'
+    template_name = 'news/list.html'
+    paginate_by = 6
+    allow_empty = True
 
 
 class NewsDetailViews(DetailView):
