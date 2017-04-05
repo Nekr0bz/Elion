bb8645f56686751a1e38f5428a1218a31fd06024
Nekr0bz <Nekr0bz@yandex.ru>
2017-04-05 17:51:20 +0300

    Ограничение доступа к страницам аутентификации. Теперь авторизированные пользователи не смогут зайти на страницы авторизации и регистрации.

--- accounts/views.py
+++ accounts/views.py
@@ -3,6 +3,7 @@ from django.views.generic.edit import FormView
 from django.views.generic.base import RedirectView
 from django.contrib.auth import logout, login as auth_login
 from django.contrib.auth.views import login as view_login
+from django.shortcuts import redirect
 from .forms import SignInForm, SignUpForm
 from generic.mixins import NextPageMixin
 
@@ -10,6 +11,12 @@ from generic.mixins import NextPageMixin
 class LoginView(NextPageMixin, FormView):
     form_class = SignInForm
     template_name = 'accounts/sign_in.html'
+
+    def get(self, request, *args, **kwargs):
+        if request.user.is_authenticated():
+            return redirect(self.get_success_url())
+
+        return super(LoginView, self).get(request, *args, **kwargs)
 
     def form_valid(self, form):
         # TODO: if user.is_active
@@ -32,12 +39,17 @@ class RegisterView(NextPageMixin, FormView):
     template_name = 'accounts/sign_up.html'
     form_class = SignUpForm
 
-    # TODO: редирект если уже авторизован
     # TODO: вывод ошибок
 
     def form_valid(self, form):
         auth_login(self.request, form.save())
         return super(RegisterView, self).form_valid(form)
+
+    def get(self, request, *args, **kwargs):
+        if request.user.is_authenticated():
+            return redirect(self.get_success_url())
+
+        return super(RegisterView, self).get(request, *args, **kwargs)
 
 
 
