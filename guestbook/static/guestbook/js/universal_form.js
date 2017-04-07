7c2b67ec08f5780f4f9c9f8ea205a0bf5be6ee68
Nekr0bz <Nekr0bz@yandex.ru>
2017-04-07 18:11:10 +0300

    Обработчик отображения формы изменения ответа на отзыв.

--- guestbook/static/guestbook/js/universal_form.js
+++ guestbook/static/guestbook/js/universal_form.js
@@ -9,6 +9,7 @@
         setUpListeners: function () {
             $('.showReply').on('click', app.showReply);
             $('a.changeReview').on('click', app.showChangeReview);
+            $('a.changeReply').on('click', app.changeReply);
         },
 
         showReply: function (e) {
@@ -41,11 +42,28 @@
             $(new_review).removeClass('hide').addClass('show');
         },
 
+        changeReply: function () {
+            app.prevChanges();
+
+            var obj_id = $(this).attr('id'),
+                new_reply = $('div[id='+obj_id+']'),
+                this_reply = $(this).parents('div.author-box'),
+                obj_text = $(this_reply).find('p').html();
+
+            $(this_reply).addClass('hide');
+            $(new_reply).find('textarea').html(obj_text).attr('placeholder', 'Ответить на отзыв клиента');
+            $(new_reply).find('button.submit').html('Сохранить').attr('id','changeReply');
+            $(new_reply).find('button.cancel').attr('id','changeReply');
+
+            $(new_reply).removeClass('hide').addClass('show');
+        },
+
         prevChanges: function () {
             $('div.form_universal.show').addClass('hide').removeClass('show');
             $('div.quote-box.hide').removeClass('hide');
+            $('div.author-box.hide').removeClass('hide');
         }
-        
+
     };
 
     app.initialize();
