624f5844679ae4acc93eaaaf601774ab3af65c51
Nekr0bz <Nekr0bz@yandex.ru>
2017-04-07 17:46:58 +0300

    Обработчик отображения формы изменения отзыва.

--- guestbook/static/guestbook/js/universal_form.js
+++ guestbook/static/guestbook/js/universal_form.js
@@ -8,19 +8,42 @@
 
         setUpListeners: function () {
             $('.showReply').on('click', app.showReply);
+            $('a.changeReview').on('click', app.showChangeReview);
         },
 
         showReply: function (e) {
             e.preventDefault();
-            $('.show').addClass('hide').removeClass('show')
+            app.prevChanges();
 
             var obj_id = $(this).attr('id'),
                 reply = $('div[id='+obj_id+']');
 
-            $(reply).removeClass('hide').addClass('show');
             $(reply).find('textarea').attr('placeholder', 'Ответить на отзыв клиента');
             $(reply).find('button.submit').html('Ответить').attr('id','showReply');
             $(reply).find('button.cancel').attr('id','showReply');
+
+            $(reply).removeClass('hide').addClass('show');
+        },
+
+        showChangeReview: function () {
+            app.prevChanges();
+
+            var obj_id = $(this).attr('id'),
+                new_review = $('div[id='+obj_id+']'),
+                this_review = $(this).parents('div.quote-box'),
+                obj_text = $(this_review).find('p').html();
+
+            $(this_review).addClass('hide');
+            $(new_review).find('textarea').html(obj_text);
+            $(new_review).find('button.submit').html('Сохранить').attr('id','showChangeReview');
+            $(new_review).find('button.cancel').attr('id','showChangeReview');
+
+            $(new_review).removeClass('hide').addClass('show');
+        },
+
+        prevChanges: function () {
+            $('div.form_universal.show').addClass('hide').removeClass('show');
+            $('div.quote-box.hide').removeClass('hide');
         }
         
     };
