0ec96fff2cf5a604521e4eacb067d8a3f9508adf
Nekr0bz <Nekr0bz@yandex.ru>
2017-04-07 18:27:04 +0300

    Обработчик кнопки "отмена".

--- guestbook/static/guestbook/js/universal_form.js
+++ guestbook/static/guestbook/js/universal_form.js
@@ -10,24 +10,23 @@
             $('.showReply').on('click', app.showReply);
             $('a.changeReview').on('click', app.showChangeReview);
             $('a.changeReply').on('click', app.changeReply);
+            $('button.cancel').on('click', app.returnChanges);
         },
 
         showReply: function (e) {
-            e.preventDefault();
-            app.prevChanges();
+            app.returnChanges(e);
 
             var obj_id = $(this).attr('id'),
                 reply = $('div[id='+obj_id+']');
 
             $(reply).find('textarea').attr('placeholder', 'Ответить на отзыв клиента');
             $(reply).find('button.submit').html('Ответить').attr('id','showReply');
-            $(reply).find('button.cancel').attr('id','showReply');
 
             $(reply).removeClass('hide').addClass('show');
         },
 
-        showChangeReview: function () {
-            app.prevChanges();
+        showChangeReview: function (e) {
+            app.returnChanges(e);
 
             var obj_id = $(this).attr('id'),
                 new_review = $('div[id='+obj_id+']'),
@@ -37,13 +36,12 @@
             $(this_review).addClass('hide');
             $(new_review).find('textarea').html(obj_text);
             $(new_review).find('button.submit').html('Сохранить').attr('id','showChangeReview');
-            $(new_review).find('button.cancel').attr('id','showChangeReview');
 
             $(new_review).removeClass('hide').addClass('show');
         },
 
-        changeReply: function () {
-            app.prevChanges();
+        changeReply: function (e) {
+            app.returnChanges(e);
 
             var obj_id = $(this).attr('id'),
                 new_reply = $('div[id='+obj_id+']'),
@@ -53,12 +51,12 @@
             $(this_reply).addClass('hide');
             $(new_reply).find('textarea').html(obj_text).attr('placeholder', 'Ответить на отзыв клиента');
             $(new_reply).find('button.submit').html('Сохранить').attr('id','changeReply');
-            $(new_reply).find('button.cancel').attr('id','changeReply');
 
             $(new_reply).removeClass('hide').addClass('show');
         },
 
-        prevChanges: function () {
+        returnChanges: function (e) {
+            e.preventDefault();
             $('div.form_universal.show').addClass('hide').removeClass('show');
             $('div.quote-box.hide').removeClass('hide');
             $('div.author-box.hide').removeClass('hide');
