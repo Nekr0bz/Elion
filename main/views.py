3b53d41ca939784329ced20038d63ca626e8e1a9
nekr0bz <nekr0bz@yandex.ru>
2017-04-11 15:43:35 +0300

    View для главное страницы

--- main/views.py
+++ main/views.py
@@ -1,5 +1,16 @@
 from django.views.generic.base import TemplateView
+from services.models import FourServiceDirection
+from about.models import Employees, StaticDates
+from news.models import News
 
 
 class MainPageView(TemplateView):
     template_name = "main/index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(MainPageView, self).get_context_data(**kwargs)
+        context['static_dates'] = StaticDates.objects.all().last()
+        context['employees'] = Employees.objects.all()
+        context['news'] = News.objects.all()[:4]
+        context['services'] = FourServiceDirection.objects.all()[:4]
+        return context
