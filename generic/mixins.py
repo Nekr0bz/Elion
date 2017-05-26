2836b44dee56a21e0281dabe372bdf13f442ebd1
Nekr0bz <Nekr0bz@yandex.ru>
2017-05-26 23:18:52 +0300

    Документация generic

--- generic/mixins.py
+++ generic/mixins.py
@@ -1,8 +1,16 @@
+# -*- coding: utf-8 -*-
 from django.views.generic.base import ContextMixin
 
 
 class NextPageMixin(ContextMixin):
+    """
+    Добавление в контекст шаблона данных
+    о старнице на которую необходимо перейти
+    """
     def get_success_url(self):
+        """
+        Возвращает страницу на которую необходимо перейти
+        """
         try:
             url = self.request.GET['next']
         except KeyError:
@@ -10,6 +18,11 @@ class NextPageMixin(ContextMixin):
         return url
 
     def get_context_data(self, **kwargs):
+        """
+        Добавляет в контекст шаблона 
+        url следующей страницы
+        :return: контекст
+        """
         context = super(NextPageMixin, self).get_context_data(**kwargs)
         context["next"] = self.get_success_url()
         return context
